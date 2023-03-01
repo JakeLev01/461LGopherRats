a7f153f969cb5d70ea6ce92f3287af9ece619041
Jake Leverett <jake.leverett24@gmail.com>
2023-03-01 12:05:20 -0600

    Added hardwareSet.py and cipher.py code

--- cipher.py
+++ cipher.py
@@ -1,1 +1,43 @@
 
+def encrypt(inputText, N, D):
+    rev = inputText[::-1]
+    listString = list(rev)
+    for i in range(len(rev)):
+        char = ord(rev[i])
+        for j in range(N):
+            if D == 1:
+                if char == 126:
+                    char = 34
+                else:
+                    char += 1
+            elif D == -1:
+                if char == 34:
+                    char = 126
+                else:
+                    char -= 1
+            listString[i] = chr(char)
+    finalString = "".join(listString)
+    return finalString
+
+
+def decrypt(inputText, N, D):
+    rev = inputText[::-1]
+    listString = list(rev)
+    for i in range(len(rev)):
+        char = ord(rev[i])
+        for j in range(N):
+            if D == 1:
+                if char == 34:
+                    char = 126
+                else:
+                    char -= 1
+            elif D == -1:
+                if char == 126:
+                    char = 34
+                else:
+                    char += 1
+            listString[i] = chr(char)
+    finalString = "".join(listString)
+    return finalString
+
+
--- hardwareSet.py
+++ hardwareSet.py
@@ -1,1 +1,54 @@
+class hardwareSet:
 
+    def __init__(self):
+        self.__capacity = 0
+        self.__availability = 0
+        self.__checkedOut = 0
+
+    def initialize_capacity(self, qty):
+        self.__capacity = qty
+        self.__availability = self.__capacity
+
+    def set_capacity(self, qty):
+        if qty > self.__capacity:
+            change = qty - self.__capacity
+            self.__availability += change
+            self.__capacity = qty
+        elif qty < self.__capacity:
+            change = self.__capacity - qty
+            self.__availability -= change
+            self.__capacity = qty
+            if self.__availability < 0:
+                self.__availability = 0
+
+    def get_availability(self):
+        return self.__availability
+
+    def get_capacity(self):
+        return self.__capacity
+
+    def check_out(self, qty):
+        if qty < 0:
+            return -1
+        elif qty > self.__availability:
+            self.__checkedOut += self.__availability
+            self.__availability = 0
+            return -1
+        else:
+            self.__availability -= qty
+            self.__checkedOut += qty
+            return 0
+
+    def check_in(self, qty):
+        if qty < 0:
+            return
+        elif (qty > self.__capacity) or (qty + self.__availability) > self.__capacity:
+            updateCheckOut = self.__capacity - self.__availability
+            self.__checkedOut -= updateCheckOut
+            self.__availability = self.__capacity
+        else:
+            self.__availability += qty
+            self.__checkedOut -= qty
+
+    def get_checkedout_qty(self):
+        return self.__checkedOut
