bbe9143672d0720f5503c34debad47b04dd47d4e
Jake Leverett <jake.leverett24@gmail.com>
2023-04-02 10:51:16 -0500

    Updated Hardware set to include flask and correct implementation

--- hardwareSet.py
+++ hardwareSet.py
@@ -1,59 +1,113 @@
 from flask import Flask, request, jsonify
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+app.route('/check_out')
+def check_out(projectID, qty, HWSet):
+    HWSetAvailability = 0
+    CheckedOut = 0
+    global collection
+    client = MongoClient("mongodb+srv://jakeleverett:rOxNEdt5txSolGvm@cluster0.ikaumwm.mongodb.net/test")
+    db = client['Projects']
+    cols = db.list_collection_names()
+
+    for col in cols:
+        doc = col.find()
+        if doc.get('ProjectID') == projectID:
+            collection = col
+            if HWSet == 1:
+                HWSetAvailability = doc.get("HWSet1Availability")
+                CheckedOut = doc.get('HWSet1CheckedOut')
+            elif HWSet == 2:
+                HWSetAvailability = doc.get("HWSet2Availability")
+                CheckedOut = doc.get('HWSet2CheckedOut')
+        break
+
+    if qty < 0:
+        return "Invalid Quantity"
+    elif qty > HWSetAvailability:
+        CheckedOut += HWSetAvailability
+        HWSetAvailability = 0
+        if HWSet == 1:
+            post = {"HWSet1Availability": HWSetAvailability, "HWSet1CheckedOut": CheckedOut}
+            db[collection].update_one(post)
+        elif HWSet == 2:
+            post = {"HWSet2Availability": HWSetAvailability, "HWSet2CheckedOut": CheckedOut}
+            db[collection].update_one(post)
+        return
+    else:
+        HWSetAvailability -= qty
+        CheckedOut += qty
+        if HWSet == 1:
+            post = {"HWSet1Availability": HWSetAvailability, "HWSet1CheckedOut": CheckedOut}
+            db[collection].update_one(post)
+        elif HWSet == 2:
+            post = {"HWSet2Availability": HWSetAvailability, "HWSet2CheckedOut": CheckedOut}
+            db[collection].update_one(post)
+
+        return 0
 
 
-class hardwareSet:
+app.route('/check_in')
+def check_in(projectID, qty, HWSet):
+    HWSetAvailability = 0
+    CheckedOut = 0
+    Capacity = 0
+    client = MongoClient("mongodb+srv://jakeleverett:rOxNEdt5txSolGvm@cluster0.ikaumwm.mongodb.net/test")
+    db = client['Projects']
+    cols = db.list_collection_names()
+    global collection
 
-    app = Flask(__name__)
+    for col in cols:
+        doc = col.find()
+        if doc.get('ProjectID') == projectID:
+            collection = col
+            if HWSet == 1:
+                HWSetAvailability = doc.get("HWSet1Availability")
+                CheckedOut = doc.get('HWSet1CheckedOut')
+            elif HWSet == 2:
+                HWSetAvailability = doc.get("HWSet2Availability")
+                CheckedOut = doc.get('HWSet2CheckedOut')
+            Capacity = doc.get('Capacity')
+        break
 
-    def __init__(self):
-        self.__capacity = 0
-        self.__availability = 0
-        self.__checkedOut = 0
 
-    def initialize_capacity(self, qty):
-        self.__capacity = qty
-        self.__availability = self.__capacity
+    if qty < 0:
+        return "Invalid Quantity"
+    elif (qty > Capacity) or (qty + HWSetAvailability) > Capacity:
+        updateCheckOut = Capacity - HWSetAvailability
+        CheckedOut -= updateCheckOut
+        HWSetAvailability = Capacity
+        if HWSet == 1:
+            post = {"HWSet1Availability": HWSetAvailability, "HWSet1CheckedOut": CheckedOut}
+            db[collection].update_one(post)
+        elif HWSet == 2:
+            post = {"HWSet2Availability": HWSetAvailability, "HWSet2CheckedOut": CheckedOut}
+            db[collection].update_one(post)
+    else:
+        HWSetAvailability += qty
+        CheckedOut -= qty
+        if HWSet == 1:
+            post = {"HWSet1Availability": HWSetAvailability, "HWSet1CheckedOut": CheckedOut}
+            db[collection].update_one(post)
+        elif HWSet == 2:
+            post = {"HWSet2Availability": HWSetAvailability, "HWSet2CheckedOut": CheckedOut}
+            db[collection].update_one(post)
 
-    def set_capacity(self, qty):
-        if qty > self.__capacity:
-            change = qty - self.__capacity
-            self.__availability += change
-            self.__capacity = qty
-        elif qty < self.__capacity:
-            change = self.__capacity - qty
-            self.__availability -= change
-            self.__capacity = qty
-            if self.__availability < 0:
-                self.__availability = 0
 
-    def get_availability(self):
-        return self.__availability
 
-    def get_capacity(self):
-        return self.__capacity
+app.route('/getProject/<int:projectID>')
+def getProject(projectID):
+    client = MongoClient("mongodb+srv://jakeleverett:rOxNEdt5txSolGvm@cluster0.ikaumwm.mongodb.net/test")
+    db = client['Projects']
+    cols = db.list_collection_names()
+    for col in cols:
+        doc = col.find()
+        if doc.get('ProjectID') == projectID:
+            post = {"ProjectID": doc.get('ProjectID'), "HWSet1Availability": doc.get('HWSet1Availability'),
+                    "HWSet2Availability": doc.get('HWSet1Availability'), "Capacity": doc.get('Capacity'),
+                    "HWSet1Checked Out": doc.get('HWSet1CheckedOut'), "HWSet2Checked Out": doc.get('HWSet2CheckedOut')}
+            return jsonify(post)
 
-    def check_out(self, qty):
-        if qty < 0:
-            return -1
-        elif qty > self.__availability:
-            self.__checkedOut += self.__availability
-            self.__availability = 0
-            return -1
-        else:
-            self.__availability -= qty
-            self.__checkedOut += qty
-            return 0
-
-    def check_in(self, qty):
-        if qty < 0:
-            return
-        elif (qty > self.__capacity) or (qty + self.__availability) > self.__capacity:
-            updateCheckOut = self.__capacity - self.__availability
-            self.__checkedOut -= updateCheckOut
-            self.__availability = self.__capacity
-        else:
-            self.__availability += qty
-            self.__checkedOut -= qty
-
-    def get_checkedout_qty(self):
-        return self.__checkedOut
+    return "Invalid projectID"
